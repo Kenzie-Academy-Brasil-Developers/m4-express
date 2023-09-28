4834a54abba47c4c878222ccc026ce1138fd98c0
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-09-28 17:32:39 -0300

    helloworld

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Olá mundo!");
+import express, { json, Request, Response } from "express";
+
+const app = express();
+
+app.use(json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Olá mundo!");
+})
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`API sucessfully started on port ${port}`);
+})
