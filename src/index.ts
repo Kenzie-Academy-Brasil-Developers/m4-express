b17634d7f52134a88de92e552a349d6790244d58
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-09-28 17:39:29 -0300

    routes

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,19 @@ const app = express();
 app.use(json());
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Olá mundo!");
+    res.send("Leitura realizada com sucesso!");
+})
+
+app.post("/", (req: Request, res: Response) => {
+    res.send("Criação realizada com sucesso!")
+})
+
+app.put("/", (req: Request, res: Response) => {
+    res.send("Atualização realizada com sucesso!")
+})
+
+app.delete("/", (req: Request, res: Response) => {
+    res.send("Exclusão realizada com sucesso!")
 })
 
 const port = 3000;
