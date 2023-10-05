82a79419bccf66ee83b6ac3e20693818c1ef4f82
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-10-05 10:17:03 -0300

    handle errors middleware and express async errors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
-import express, { json, Request, Response } from "express";
+import "express-async-errors";
+import express, { json } from "express";
 import { productsRouter } from "./routes/products.routes";
+import { HandleErrors } from "./errors/handleErrors.middleware";
 
 const app = express();
 
 app.use(json());
 
 app.use("/products", productsRouter);
+
+app.use(HandleErrors.execute);
 
 const port = 3000;
 
