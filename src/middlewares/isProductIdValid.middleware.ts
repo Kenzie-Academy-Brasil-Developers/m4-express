9c8585df74fb8180d38223a53ef32d0198f2a2e0
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-10-05 09:47:13 -0300

    app errors class

--- src/middlewares/isProductIdValid.middleware.ts
+++ src/middlewares/isProductIdValid.middleware.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import { productsDatabase } from "../database/database";
+import { AppError } from "../errors/AppError";
 
 export class IsProductIdValid{
     static execute(req: Request, res: Response, next: NextFunction){
         if(!productsDatabase.some(product => product.id === Number(req.params.id))){
-            return res.status(404).json({ message: "Product not found."});
+            throw new AppError(404, "Product not found.");
         }
 
         next();
