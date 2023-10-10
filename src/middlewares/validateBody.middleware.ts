64d505a21f7d5ab218f86303cc7523f06d2239a0
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-10-10 09:48:25 -0300

    zod and handleerrors

--- src/middlewares/validateBody.middleware.ts
+++ src/middlewares/validateBody.middleware.ts
@@ -1,34 +1,28 @@
 import { NextFunction, Request, Response } from "express";
 import { AnyZodObject, ZodError } from "zod";
 
-interface IRequestSchemas{
-    params?: AnyZodObject;
-    body?: AnyZodObject;
-    query?: AnyZodObject;
+interface IRequestSchemas {
+   params?: AnyZodObject;
+   body?: AnyZodObject;
+   query?: AnyZodObject;
 }
 
-export class ValidateBody{
-    static execute(schemas: IRequestSchemas){
-        return async (req: Request, res: Response, next: NextFunction) => {
-            try {
-                if(schemas.params){
-                    req.params = await schemas.params.parseAsync(req.params);
-                }
+export class ValidateBody {
+   static execute(schemas: IRequestSchemas) {
+      return async (req: Request, res: Response, next: NextFunction) => {
+         if (schemas.params) {
+            req.params = await schemas.params.parseAsync(req.params);
+         }
 
-                if(schemas.body){
-                    req.body = await schemas.body.parseAsync(req.body);
-                }
+         if (schemas.body) {
+            req.body = await schemas.body.parseAsync(req.body);
+         }
 
-                if(schemas.query){
-                    req.query = await schemas.query.parseAsync(req.query);
-                }                
-    
-                next();
-            } catch (error) {
-                if(error instanceof ZodError){
-                    return res.status(409).json(error); 
-                }                
-            }  
-        }            
-    }
+         if (schemas.query) {
+            req.query = await schemas.query.parseAsync(req.query);
+         }
+
+         next();
+      };
+   }
 }
